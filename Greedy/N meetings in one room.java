30077ee8c77feb7725947c3f53f32cd015457119
Rohit <90521597+Rohit311098@users.noreply.github.com>
2021-12-25 23:14:16 +0530

    Update N meetings in one room.java

--- Greedy/N meetings in one room.java
+++ Greedy/N meetings in one room.java
@@ -5,17 +5,21 @@ class Solution
 {
     //Function to find the maximum number of meetings that can
     //be performed in a meeting room.
-    public static class Interval {
-      int start, end;
-      Interval(int start, int end) {
-          this.start = start;
-          this.end = end;
-      }
-  }
-  
-  public static class Mycomparator implements Comparator<Interval>
+    public static class Interval
     {
-        public int compare(Interval obj1,Interval obj2)
+        int start;
+        int end;
+        
+        Interval(int start,int end)
+        {
+            this.start=start;
+            this.end=end;
+        }
+    }
+    
+    public static class MyComparator implements Comparator<Interval>
+    {
+        public  int compare(Interval obj1,Interval obj2)
         {
             if(obj1.end!=obj2.end)
             {
@@ -24,30 +28,27 @@ class Solution
             return obj1.start-obj2.start;
         }
     }
-    
     public static int maxMeetings(int start[], int end[], int n)
     {
         // add your code here
-        
         Interval[] intervals=new Interval[n];
         for(int i=0;i<n;i++)
         {
             intervals[i]=new Interval(start[i],end[i]);
         }
         
-        Arrays.sort(intervals, new Mycomparator());
+        Arrays.sort(intervals,new MyComparator());
         
-        int count=1;
-        int last=intervals[0].end;
-        for(int i=1;i<n;i++)
+        int count=0;
+        int lastend=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++)
         {
-                 if(last<intervals[i].start)
-                 {
-                     last=intervals[i].end;
-                     count++;
-                 }
+            if(lastend<intervals[i].start)
+            {
+                count++;
+                lastend=intervals[i].end;
+            }
         }
-
         return count;
     }
 }
