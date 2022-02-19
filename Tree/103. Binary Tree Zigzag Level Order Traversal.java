7521cb44cf4e8af0d4ee10d430403a96b0823139
Rohit <90521597+Rohit311098@users.noreply.github.com>
2022-02-19 12:58:50 +0530

    Create 103. Binary Tree Zigzag Level Order Traversal.java

---
+++ Tree/103. Binary Tree Zigzag Level Order Traversal.java
@@ -0,0 +1,81 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        
+         List<List<Integer>> result=new ArrayList<>();
+        
+        if(root==null)
+        {
+            return result;
+        }
+        Stack<TreeNode> stk1=new Stack<>();
+        Stack<TreeNode> stk2=new Stack<>();
+        
+        int level=1;
+        
+        stk1.push(root);
+        
+        List<Integer> currlevel=new ArrayList<>();
+        while(stk1.size()==0 && stk2.size()==0)
+        {
+            if(level%2==1)
+            {
+                //odd level mai first stack se second stack mai daalege
+                while(stk1.size()>0)
+                {
+                    TreeNode ele=stk1.pop();
+                    currlevel.add(ele.val);
+                    
+                    if(ele.left!=null)
+                    {
+                        stk2.push(ele.left);
+                    }
+                    
+                    if(ele.right!=null)
+                    {
+                        stk2.push(ele.right);
+                    }
+                }
+            }
+             else
+            {
+                //odd level mai first stack se second stack mai daalege
+                while(stk2.size()>0)
+                {
+                    TreeNode ele=stk2.pop();
+                    currlevel.add(ele.val);
+                    
+                    if(ele.right!=null)
+                    {
+                        stk1.push(ele.right);
+                    }
+                    if(ele.left!=null)
+                    {
+                        stk1.push(ele.left);
+                    }
+                } 
+            }
+            result.add(currlevel);
+            currlevel=new ArrayList<>();
+            level++;
+        }
+        
+        return result;
+        
+        
+    }
+}
