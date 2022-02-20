63dea33a1b02200a8e73d7b0a3b99233c7695ea5
Rohit <90521597+Rohit311098@users.noreply.github.com>
2022-02-20 17:51:57 +0530

    Update Diagonal Traversal of Binary Tree.java

--- Tree/Diagonal Traversal of Binary Tree.java
+++ Tree/Diagonal Traversal of Binary Tree.java
@@ -9,13 +9,15 @@ class Tree
            queue.add(root);
            Node curr=null;
            
-           while(curr!=null || queue.size()==0)
+           while(curr!=null || queue.size()>0)
            {
                if(curr==null)
                {
                    curr=queue.remove();
                    continue;
                }
+               
+               result.add(curr.data);
                if(curr.left!=null)
                {
                    queue.add(curr.left);
