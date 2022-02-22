f7dfb2b3397033f8f4fd8948ba825c266c5ed9dd
Rohit <90521597+Rohit311098@users.noreply.github.com>
2022-02-22 23:47:26 +0530

    Create 863. All Nodes Distance K in Binary Tree.java

---
+++ Tree/863. All Nodes Distance K in Binary Tree.java
@@ -0,0 +1,84 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public  ArrayList<TreeNode> getpath(TreeNode root,TreeNode target)
+    {
+        if(root==null)
+        {
+            return new ArrayList<>();
+        }
+        if(root.val==target.val)
+        {
+            ArrayList<TreeNode> arr=new ArrayList<>();
+            arr.add(root);
+            return arr;
+        }
+        
+        ArrayList<TreeNode> leftside=getpath(root.left,target);
+        if(leftside.size()!=0)
+        {
+            leftside.add(0,root);
+            return leftside;
+        }
+        
+        ArrayList<TreeNode> rightside=getpath(root.right,target);
+        if(rightside.size()!=0)
+        {
+            rightside.add(0,root);
+            return rightside;
+        }
+        
+        
+        return new ArrayList<>();
+    }
+    public void kleveldown(TreeNode root,TreeNode blocker,int k,List<Integer> result)
+     {
+         if(root==null || root==blocker)
+         {
+             return ;
+         }
+         if(k==0)
+         {
+             result.add(root.val);
+             return;
+         }
+         kleveldown(root.left,blocker,k-1,result);
+         kleveldown(root.right,blocker,k-1,result);
+     }
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        
+        if(root==null)
+        {
+            return null;
+        }
+        
+        List<Integer> result=new ArrayList<>();
+        ArrayList<TreeNode> root2node=new ArrayList<>();
+        
+        root2node=getpath(root,target);
+        
+        //System.out.println(root2node);
+        //null esliye add kiya hai taki last wale element k liye blocker null ho
+        root2node.add(null);
+        
+        int n=root2node.size();
+        for(int i=n-2;i>=0;i--)
+        {
+            TreeNode curr=root2node.get(i);
+            
+            TreeNode blocker=root2node.get(i+1);
+            
+            kleveldown(curr,blocker,k,result);
+            k--;
+        }
+        
+        return result;
+    }
+}
