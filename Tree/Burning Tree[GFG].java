7d1125e6ec50c9179e644ea4ad039a53f6778026
Rohit <90521597+Rohit311098@users.noreply.github.com>
2022-02-23 01:51:04 +0530

    Update Burning Tree[GFG].java

--- Tree/Burning Tree[GFG].java
+++ Tree/Burning Tree[GFG].java
@@ -11,7 +11,7 @@ class Solution
     		right = null;
     	}
     }*/
-    static int time=0;
+    static int mintime=0;
      public  static int dfs(Node root,int target)
     {
         if(root==null)
@@ -20,41 +20,38 @@ class Solution
         }
         if(root.data==target)
         {
-            //-1 esliye return kiya hai because height in terms of nodes hai but haime in terms of edges chahiye from current node
-            time=Math.max(time,height(root,null)-1);
+            height(root,null,0);
             return 1;
         }
         
         int left=dfs(root.left,target);
         if(left>0)
         {
-            //-1 esliye return kiya hai because height in terms of nodes hai but haime in terms of edges chahiye from current node
-            time=Math.max(time,left+height(root,root.left)-1);
+           height(root,root.left,left);
             return 1+left;
         }
         
         int right=dfs(root.right,target);
         if(right>0)
         {
-            //-1 esliye return kiya hai because height in terms of nodes hai but haime in terms of edges chahiye from current node
-            time=Math.max(time,right+height(root,root.right)-1);
+            height(root,root.right,right);
             return 1+right;
         }
         return -1;
     }
-    public static int height(Node root,Node blocker)
+    public static int height(Node root,Node blocker,int time)
      {
          if(root==null || root==blocker)
          {
              return 0;
          }
-         return 1+Math.max(height(root.left,blocker),height(root.right,blocker));
+         mintime=Math.max(mintime,time);
+         return 1+Math.max(height(root.left,blocker,time+1),height(root.right,blocker,time+1));
      }
     public static int minTime(Node root, int target) 
     {
-       time=0;
-       
+       mintime=0;
        dfs(root,target);
-       return time;
+       return mintime;
     }
 }
