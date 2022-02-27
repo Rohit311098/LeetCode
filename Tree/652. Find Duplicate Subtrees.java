eb18c4d30c8e4948116714daf987adba303bb8cb
Rohit <90521597+Rohit311098@users.noreply.github.com>
2022-02-27 17:44:12 +0530

    Create 652. Find Duplicate Subtrees.java

---
+++ Tree/652. Find Duplicate Subtrees.java
@@ -0,0 +1,36 @@
+TC-O(N) SC-O(N)
+class Solution {
+    
+    HashMap<String,Integer> h;
+    List<TreeNode> result;
+    public String helper(TreeNode root)
+    {
+        if(root==null)
+        {
+            return "#";   
+        }
+        
+        String curr=root.val+","+helper(root.left)+","+helper(root.right);
+        
+        
+        h.put(curr,h.getOrDefault(curr,0)+1);
+        if(h.get(curr)==2)
+        {
+            result.add(root);
+        }
+        
+        return curr;
+    }
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        
+        if(root==null)
+        {
+            return new ArrayList<>();
+        }
+        
+        h=new HashMap<>(); 
+        result=new ArrayList<>();
+        helper(root);
+        return result;
+    }
+}
