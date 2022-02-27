5dd67790a3b77e75407ef19931fb3ea722342904
Rohit <90521597+Rohit311098@users.noreply.github.com>
2022-02-27 13:54:58 +0530

    Update Diagonal Traversal of Binary Tree.java

--- Tree/Diagonal Traversal of Binary Tree.java
+++ Tree/Diagonal Traversal of Binary Tree.java
@@ -2,28 +2,27 @@ class Tree
 {
      public ArrayList<Integer> diagonal(Node root)
       {
-           ArrayList<Integer> result=new ArrayList<>();
-           
-           Queue<Node> queue=new ArrayDeque<>();
-           
-           queue.add(root);
-           Node curr=null;
-           
-           while(curr!=null || queue.size()>0)
-           {
-               if(curr==null)
-               {
-                   curr=queue.remove();
-                   continue;
-               }
-               
-               result.add(curr.data);
-               if(curr.left!=null)
-               {
-                   queue.add(curr.left);
-               }
-               curr=curr.right;
-           }
-           return result;
+          ArrayList<Integer> result=new ArrayList<>();
+          
+          Queue<Node> queue=new ArrayDeque<>();
+          
+          Node curr=null;
+          queue.add(root);
+          while(curr!=null || queue.size()>0)
+          {
+              curr=queue.remove();
+              
+              while(curr!=null)
+              {
+                  result.add(curr.data);
+                  if(curr.left!=null)
+                  {
+                      queue.add(curr.left);
+                  }
+                  curr=curr.right;
+              }
+          }
+          
+          return result;
       }
 }
