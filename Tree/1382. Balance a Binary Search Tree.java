61c16fced160b0b271e74e6c4495a8d723310e06
Rohit <90521597+Rohit311098@users.noreply.github.com>
2022-03-20 21:54:13 +0530

    Create 1382. Balance a Binary Search Tree.java

---
+++ Tree/1382. Balance a Binary Search Tree.java
@@ -0,0 +1,38 @@
+class Solution {
+    public void getinorder(TreeNode root,ArrayList<Integer> arr)
+    {
+        if(root==null)
+        {
+            return;
+        }
+        
+        getinorder(root.left,arr);
+        arr.add(root.val);
+        getinorder(root.right,arr);
+    }
+    
+    public TreeNode balanceBSTHelper(ArrayList<Integer> arr,int left,int right)
+    {
+        if(left>right)
+        {
+            return null;
+        }
+        
+        int mid=left+(right-left)/2;
+        
+        TreeNode root=new TreeNode(arr.get(mid));
+        root.left=balanceBSTHelper(arr,left,mid-1);
+        root.right=balanceBSTHelper(arr,mid+1,right);
+        return root;
+    }
+    public TreeNode balanceBST(TreeNode root) {
+        
+        ArrayList<Integer> arr=new ArrayList<>();
+        
+        getinorder(root,arr);
+        
+        System.out.println(arr);
+        return balanceBSTHelper(arr,0,arr.size()-1);
+        
+    }
+}
