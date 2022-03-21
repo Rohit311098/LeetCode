697db45da84b683799084567115511778687b0fe
Rohit <90521597+Rohit311098@users.noreply.github.com>
2022-03-21 22:45:47 +0530

    Update 474 · Lowest Common Ancestor II[LintCode].java

--- Tree/474 · Lowest Common Ancestor II[LintCode].java
+++ Tree/474 · Lowest Common Ancestor II[LintCode].java
@@ -1,4 +1,4 @@
-1. Approach 1 Using HashSet
+1. Brute Force TC-O(N)    SC-O(N) Extra Space HashSet
 /**
  * Definition of ParentTreeNode:
  * 
@@ -45,3 +45,71 @@ public class Solution {
         return ans;
     }
 }
+
+2. TC-O(H)  SC-O(1)   Distance wale method se karege
+
+public class Solution {
+    /*
+     * @param root: The root of the tree
+     * @param A: node in the tree
+     * @param B: node in the tree
+     * @return: The lowest common ancestor of A and B
+     */
+    public ParentTreeNode lowestCommonAncestorII(ParentTreeNode root, ParentTreeNode A, ParentTreeNode B) {
+        
+        int d1=0,d2=0;
+
+        ParentTreeNode temp1=A;
+        while(temp1!=null)
+        {
+            d1++;
+            temp1=temp1.parent;
+        }
+
+        ParentTreeNode temp2=B;
+        while(temp2!=null)
+        {
+            d2++;
+            temp2=temp2.parent;
+        }
+
+        if(d1==d2)
+        {
+            return A.parent;
+        }
+        else if(d1>d2)
+        {
+            temp1=A;
+            temp2=B;
+            while(d1!=d2)
+            {
+                temp1=temp1.parent;
+                d1--;
+            }
+        }
+        else
+        {
+            temp1=A;
+            temp2=B;
+            while(d1!=d2)
+            {
+                temp2=temp2.parent;
+                d2--;
+            }
+        }
+
+        while(temp1!=null && temp2!=null)
+        {
+            if(temp1==temp2)
+            {
+                break;
+            }
+
+            temp1=temp1.parent;
+            temp2=temp2.parent;
+        }
+
+        return temp1;
+    }
+}
+    
