1f649191401323737a4f82bda7411252a3c99f6c
Rohit <90521597+Rohit311098@users.noreply.github.com>
2022-03-27 10:17:28 +0530

    Update DFS of Graph.java

--- Graph/DFS of Graph.java
+++ Graph/DFS of Graph.java
@@ -1,3 +1,5 @@
+1. Using Recursive call Stack
+
 class Solution {
     // Function to return a list containing the DFS traversal of the graph.
     public ArrayList<Integer> dfsOfGraph(int v, ArrayList<ArrayList<Integer>> adj) {
@@ -30,3 +32,44 @@ class Solution {
     }
     
 }
+
+2. Using Iterative Method at Prepcoding Portal
+public static class Pair
+   {
+       int src;
+       String path;
+       
+       Pair(int src,String path)
+       {
+           this.src=src;
+           this.path=path;
+       }
+   }
+   public static void iterative(int src,ArrayList<Edge>[] graph)
+   {
+       boolean visited[]=new boolean[graph.length];
+       Stack<Pair> stk=new Stack<>();
+       stk.push(new Pair(src,src+""));
+       
+       while(stk.size()>0)
+       {
+           Pair top=stk.pop();
+           
+           if(visited[top.src]==true)
+           {
+               continue;
+           }
+           
+           visited[top.src]=true;
+           
+           System.out.println(top.src+"@"+top.path);
+           
+           for(Edge e:graph[top.src])
+           {
+               if(visited[e.nbr]==false)
+               {
+                   stk.push(new Pair(e.nbr,top.path+e.nbr));
+               }
+           }
+       }
+   }
