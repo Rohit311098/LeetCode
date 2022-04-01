c5f62cc5c15ace0bcabe7247fdae73a49907ac8c
Rohit <90521597+Rohit311098@users.noreply.github.com>
2022-04-01 22:23:45 +0530

    Update 938. Range Sum of BST.java

--- Tree/938. Range Sum of BST.java
+++ Tree/938. Range Sum of BST.java
@@ -1,3 +1,4 @@
+1. Using Auxillary Stack
 class Solution {
     int sum=0;
     public int rangeSumBST(TreeNode root, int low, int high) {
@@ -13,6 +14,45 @@ class Solution {
         rangeSumBST(root.right,low,high);
         
         return sum;
+        
+    }
+}
+
+2. Using Stack
+class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        
+        if(root==null)
+        {
+            return 0;
+        }
+        
+        int count=0;
+        
+        Stack<TreeNode> stk=new Stack<>();
+        stk.push(root);
+        
+        while(stk.size()>0)
+        {
+            TreeNode top=stk.pop();
+            
+            if(top.val>=low && top.val<=high)
+            {
+               count+=top.val;   
+            }
+            
+            if(top.left!=null && top.val>low)
+            {
+                stk.push(top.left);
+            }
+            
+            if(top.right!=null && top.val<high)
+            {
+                stk.push(top.right);
+            }
+        }
+        
+        return count;
         
     }
 }
